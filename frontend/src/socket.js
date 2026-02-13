55c5c22bf58f3fa5d0a72ac6b7e2206b088f47fa
Archi <kandratikas@gmail.com>
2026-02-13 15:22:58 +0300

    socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 const SERVER_URL = window.location.hostname === "localhost" 
     ? "http://localhost:3000"
-    : `${window.location.origin}`;
+    : "https://tic-tac-toe-app-backend-i1z5.onrender.com";
 
 export const socket = io(SERVER_URL);
